04a57b46073b7babbf6dea7b4dfe446946401cea
Chen, Ron Gang <ron.chengang@gmail.com>
2024-11-08 17:44:53 +0800

    add duration calculation sql for mysql 5.7

--- scripts/aggregate_durations_mysql.sql
+++ scripts/aggregate_durations_mysql.sql
@@ -11,3 +11,27 @@ FROM (SELECT project,
       WINDOW w AS (ORDER BY time)) s2
 WHERE diff IS NOT NULL
 GROUP BY project, language, editor, operating_system, machine, branch;
+
+-- for mysql 5.7
+-- SELECT project, language, editor, operating_system, machine, branch, SUM(GREATEST(1, diff)) as 'sum'
+-- FROM (
+--          SELECT
+--              h1.project,
+--              h1.language,
+--              h1.editor,
+--              h1.operating_system,
+--              h1.machine,
+--              h1.branch,
+--              TIME_TO_SEC(LEAST(TIMEDIFF(h1.time,
+--                                         (SELECT h2.time
+--                                          FROM heartbeats h2
+--                                          WHERE h2.time < h1.time
+--                                            AND h2.user_id = h1.user_id
+--                                          ORDER BY h2.time DESC
+--                                          LIMIT 1)
+--                                ), '00:02:00')) as 'diff'
+--          FROM heartbeats h1
+--          WHERE h1.user_id = 'ron'
+--      ) s2
+-- WHERE diff IS NOT NULL
+-- GROUP BY project, language, editor, operating_system, machine, branch;
