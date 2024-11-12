f64ee21f92a867e82bae20a10ab2efb464559200
Chen, Ron Gang <ron.chengang@gmail.com>
2024-11-12 23:20:32 +0800

    feat: take first 8 of commitid

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-commit_hash=$(git rev-parse HEAD)
+commit_hash=$(git rev-parse --short=8 HEAD)
 output_file="wakapi.$commit_hash"
 
 go build -ldflags "-X github.com/muety/wakapi/utils.CommitHash=$commit_hash" -o $output_file
